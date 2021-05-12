e5130d05ec8ab5c9cd701cb531883dcea6e82755
Drish-xD <79963160+Drish-xD@users.noreply.github.com>
2021-05-12 19:16:19 +0530

    Website update 1.1

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Animations
 
 AOS.init({
-
-    offset: 200,
+    delay: 200,
+    offset: 150,
     duration: 1000,
     once: false,
-    anchorPlacement: 'top-center',
+    anchorPlacement: 'center-bottom',
 });
 
 
-
 // smooth scroll 
+
 
 $('.nav-bar a').on('click', function(e){
     if(this.hash !==''){
@@ -25,15 +25,18 @@ $('.nav-bar a').on('click', function(e){
 });
 
 
-
 // navigation bar animation
+
+
 $(document).ready(function() {
     $(".menu-icon").on("click", function() {
           $("nav ul").toggleClass("showing");
     });
 });
 
-// Scrolling Effect
+
+// Scrolling Effect text
+
 
 $(window).on("scroll", function() {
     if($(window).scrollTop()) {
@@ -44,3 +47,21 @@ $(window).on("scroll", function() {
           $('.nav-bar').removeClass('black');
     }
 })
+
+
+// menu bar animation
+
+
+var wrapperMenu = document.querySelector('.wrapper-menu');
+
+  wrapperMenu.addEventListener('click', function(){
+    wrapperMenu.classList.toggle('open');  
+})
+
+
+// scroll effect of skill bars
+
+
+ScrollOut({
+    targets: '.bar,skill-bars,.progress-line,.skill span'
+  });
