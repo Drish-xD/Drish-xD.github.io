50c6860407e5345428d6f37b41721cec15952ae3
Drish <drish.xd@gmail.com>
2021-11-29 18:47:09 +0530

    added 404

---
+++ src/components/404.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <h2>Page Not Found</h2>
+    <img src="../assets/img/Error.svg" alt="404" />
+    <a href="https://drish-xd.is-a.dev/Portfolio">Go Home</a>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+section {
+  width: 100%;
+  min-height: 100vh;
+  display: grid;
+  place-items: center center;
+  place-content: center center;
+}
+
+img {
+  width: 80%;
+  height: auto;
+}
+
+h2 {
+  padding: 25px 0 0 0;
+}
+
+a {
+  font: 600 18px "Syne";
+  text-decoration: none;
+  letter-spacing: 0.15rem;
+  background-color: #243038b3;
+  border-radius: 50px;
+  border: 2px solid #7e57c2;
+  color: #f1f1f1;
+  padding: 5px 20px;
+  cursor: pointer;
+  margin-bottom: 25px;
+  transition: box-shadow 0.5s, color 0.55s;
+}
+
+a:hover {
+  box-shadow: inset 0 0 10px 20px #7e56c2b3;
+  color: #0c0c0c;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Error from "../components/404.vue";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Portfolio.vue"),
+  },
+  {
+    path: "/*",
+    name: "Error",
+    component: Error,
   },
 ];
 
